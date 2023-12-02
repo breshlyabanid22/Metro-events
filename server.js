4b3cb7c496cb29e076947f3652be0cecb32e8da2
Breshly U. Abanid <114142632+breshlyabanid22@users.noreply.github.com>
2023-12-02 13:24:12 +0800

    Add files via upload
    These are the files inside the backend folder. NOTE: Also, install the npm

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "", 
-    database: "signup"
+    password: "Lordmarkpapa23!", 
+    database: "signup",
+    port: "3307"
 })
 
 app.post('/signup', (req, res) => {
@@ -39,9 +40,9 @@ app.post('/login', (req, res) => {
         console.log(data.length);
         if (data.length > 0) {
             if(req.body.email[0] === 'admin@gmail.com' && req.body.password[0] === '123'){
-               return res.json("admin_access"); 
+               return res.json({userType: "admin_access"}); 
             }else{
-                return res.json("Success");
+                return res.json({userType: "user"});
             }
         } else {
             return res.json("Failed");
@@ -59,6 +60,23 @@ app.get('/admin/users', (req, res) => {
         return res.json(data);
     });
 });
+app.post('/api/addEvent', (req, res) => {
+    const { title, start, end } = req.body;
+  
+    // Assuming your SQL query for inserting data looks like this
+    const sql = 'INSERT INTO events (`title` , `dates` ) VALUES (?, ?)';
+    const values = [title, `${start} to ${end}`]; // Concatenate start and end dates or format as needed
+    
+    db.query(sql, values, (err, result) => {
+      if (err) {
+        console.error('Error:', err);
+        return res.json({ success: false, message: 'Error adding event to the database' });
+      }
+  
+      console.log('Event added to the database:', result);
+      return res.json({ success: true, message: 'Event added to the database' });
+    });
+  });
 
 
 app.listen(8081, ()=> {
